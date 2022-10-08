9c15bf64c6601ecad0ba62de8e74f23d07897bf5
Mike <mik@DESKTOP-OI5ALMT.localdomain>
2022-10-08 11:05:54 -0500

    Config file added

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+define('URL', 'dir');
+define('REGX_EMAIL', '/^([a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3})$/');
+define('REGX_NKNAME', "/^([a-zA-z0-9]{3,})$/");
+define('REGX_PWD',  "/^((?=.+[A-Za-z])(?=.+\d)(?=.+[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,})$/");
+define('REGX_NME', "/^[a-zA-Z\s]*$/");
+//dir FOR NGINX
+//url FOR APACHE
+
+define('SNAME', 'localhost');
+define('UNAME', 'root');
+define('DNAME', 'MERCADONA_DB');
+define('PWD',  'quienSoy?Miguel9k');
+
+define('API', 'http://localhost');
+?>
